a7d85e5275e46f7a33e300456ef5b60e95ab74ba
gravis <tomaranai.baburuu@gmail.com>
2024-02-10 12:21:05 +0900

    強制労働なう

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ bot = commands.Bot("!", intents=discord.Intents.all())
 
 @bot.tree.command(name="5000yen")
 async def yen_generater(interaction: discord.Interaction, top: str, bottom: str):
+    top = top.replace(" ", "%20")
+    bottom = bottom.replace(" ", "%20")
     await interaction.response.send_message(f"https://gsapi.cbrx.io/image?top={top}&bottom={bottom}")
 
 @bot.event
